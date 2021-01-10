62a72e9c9bb77239e8e1456b6bf05b535b016e5e
Zehra <zehrabilgin341@gmail.com>
2021-01-10 17:32:39 +0300

    Array'de bulunan sehrin index degerini yazdirma

---
+++ SehirIndex/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SehirIndex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] sehirler = { "Ankara", "Kayseri", "Eskisehir", "Trabzon", "Gaziantep", "Mugla", "Erzurum", "Kutahya" };
+            Console.WriteLine("Bir sehir giriniz:");
+            string input = Console.ReadLine();
+            bool var = false;
+
+            for (int i = 0; i < sehirler.Length; i++)
+            {
+                if (sehirler[i].Equals(input))
+                {
+                    Console.WriteLine(input + " listede var.");
+                    Console.WriteLine("Listede bulunduğu konum: " + i);
+                    var = true;
+                }
+            }
+
+            if (var == false) Console.WriteLine("Sehir listede bulunamadı.");
+            Console.ReadLine();
+
+        }
+    }
+}
