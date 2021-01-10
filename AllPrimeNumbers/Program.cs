6eb08459f6f364cad7f005277fa4bdf6e01d137f
Zehra <zehrabilgin341@gmail.com>
2021-01-10 17:27:31 +0300

    2 ve 10.000 arasındaki tüm asal sayılar

---
+++ AllPrimeNumbers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllPrimeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("2 ile 10.000 arasındaki asal sayılar:");
+            for (int input = 2; input < 10000; input++)
+            { 
+                bool asal = true;
+                for(int bolen = 2; bolen < input; bolen++)
+                {
+                    if(input % bolen == 0)
+                    {
+                        asal = false;
+                        break;
+                    }
+
+
+                }
+                if (asal)
+                {
+                    Console.WriteLine(input);
+                }
+            }
+
+            Console.ReadLine();
+            
+        }
+    }
+}
